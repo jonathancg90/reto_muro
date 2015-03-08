35fdb84ecae9d3475f75085903e01b7fee2672c4
jonathancg90 <jonathancg90@gmail.com>
2015-03-08 15:35:42 -0500

    uncomment middleware

--- app/middlewares/authentication.js
+++ app/middlewares/authentication.js
@@ -8,12 +8,12 @@ var publicPaths = [
 
 module.exports = function() {
     return function(req, res, next) {
-    	// if ( publicPaths.indexOf(req.path)>=0 || req.isAuthenticated() ) {
-     //        res.locals.user = req.user || {};
+    	if ( publicPaths.indexOf(req.path)>=0 || req.isAuthenticated() ) {
+            // res.locals.user = req.user || {};
     		return next();	
-    	// } else { 
-     //        req.flash('message', 'Usted no tiene permiso de acceder')
-    	// 	res.redirect('/');
-    	// }
+    	} else { 
+            req.flash('message', 'Usted no tiene permiso de acceder')
+    		res.redirect('/');
+    	}
     }
 };
